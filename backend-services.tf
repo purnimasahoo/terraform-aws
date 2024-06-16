81be11b6b3ee24adb78a156d46867fbc328b289a
Purnima Sahoo <purnima.sahoo@gmail.com>
2024-06-16 16:33:07 -0700

    Backend service TF

--- backend-services.tf
+++ backend-services.tf
@@ -16,12 +16,11 @@ resource "aws_db_instance" "vprofile-rds" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.6.34"
-  instance_class         = "db.t2.micro"
-  name                   = var.dbname
+  engine_version         = "5.7.44"
+  instance_class         = "db.t3.micro"
   username               = var.dbuser
   password               = var.dbpass
-  parameter_group_name   = "default.mysql5.6"
+  parameter_group_name   = "default.mysql5.7"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
@@ -34,7 +33,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.5"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
   security_group_ids   = [aws_security_group.vprofile-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.vprofile-ecache-subgrp.name
@@ -43,7 +42,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
 resource "aws_mq_broker" "vprofile-rmq" {
   broker_name        = "vprofile-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
+  engine_version     = "5.15.16"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vprofile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
